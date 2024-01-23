c2de38a2460a083d485eecdc99171c045aa160a5
Anish jha <122715402+anish-stack@users.noreply.github.com>
2024-01-23 13:29:25 +0530

    adding-somethings--vercel-file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 export default {
   plugins: [react()],
   build: {
-    outDir: './dist', // or the directory you want to use
+    outDir: './build', // or the directory you want to use
   },
 };
